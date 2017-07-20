cfc886818537618c201da11c2a86bac45c4027b6
Mahdi <Mahdi@MAHDI-PC>
2017-07-20 11:35:37 +0430

    Updated Devexpress Library 17.1

--- Build.fsx
+++ Build.fsx
@@ -65,6 +65,7 @@ Target "Package" (fun _ ->
     GeneratePackage "FarsiLibrary.Win"
     GeneratePackage "FarsiLibrary.Win.DevExpress.15.2"
     GeneratePackage "FarsiLibrary.Win.DevExpress.16.1"
+    GeneratePackage "FarsiLibrary.Win.DevExpress.17.1"
 )
 
 "EnsureDir"
